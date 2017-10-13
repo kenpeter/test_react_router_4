357b013853dd29b123aa8ac0b7b6bf6e0d7559c9
kenpeter <kenpeter>
2017-10-13 13:46:39 +1100

    update

--- src/app/actions/booksAction.js
+++ src/app/actions/booksAction.js
@@ -33,8 +33,8 @@ export function fetchBooks() {
       return json;
     })
     .then((json) => {
-      console.log('so....');
-      console.log(json);
+      //console.log('so....');
+      //console.log(json);
       dispatch(getBooksAction(json));
     });
   };
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,15 +6,45 @@ import {bindActionCreators} from 'redux';
 
 class Home extends Component{
 
+  // This time bind this works.
   myclick() {
     this.props.booksAction.fetchBooks();
   }
 
+  displayData() {
+    let books = this.props.books.data;
+    let output = '';
+    if(books) {
+      if(books.length === 0) {
+        return output;
+      } else {
+        output = books.map((item, index) => {
+          console.log('...');
+          console.log(item.first_name);
+          let first_name = item.first_name;
+          return (
+            <li key={index}>{item.first_name}</li>
+          );
+        });
+
+        output = <ul>{output}</ul>;
+        return output;
+      }
+    } else {
+      return '';
+    }
+  }
+
   render(){
+    //console.log(this.props.books);
+
     return(
       <div>
         <h2>Home</h2>
-        <button onClick={this.myclick.bind(this)}>Click!!!!</button>
+        {this.displayData()}
+        <p>
+          <button onClick={this.myclick.bind(this)}>Click!!!!</button>
+        </p>
       </div>
     );
   }
